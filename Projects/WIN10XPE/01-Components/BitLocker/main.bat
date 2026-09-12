9bb25dc82f462bf4cf9ecb976c7f7c4a66ee243d
slore <slorelee@gmail.com>
2026-09-12 17:51:07 +0800

    update add encrypt-bde menu (BitLocker)

--- Projects/WIN10XPE/01-Components/BitLocker/main.bat
+++ Projects/WIN10XPE/01-Components/BitLocker/main.bat
@@ -4,11 +4,18 @@ rem Explorer BitLocker integration
 rem full feature
 rem call AddFiles "@\Windows\System32\#nbde*.exe,fve*.exe,bde*.dll,fve*.dll,BitLocker*.*,EhStor*.*"
 
+rem unlock
 call AddFiles "bdesvc.dll,bdeunlock.exe,fvenotify.exe,Windows.UI.Immersive.dll"
 
 rem add for boot.wim
 call AddFiles "EhStorAPI.dll,EhStorAuthn.exe,EhStorPwdMgr.dll,EhStorShell.dll"
 call AddFiles "bdeui.dll,fveapi.dll,fvecerts.dll,fveui.dll"
+
+rem encrypt
+call AddFiles "BitLockerWizard.exe,BdeHdCfgLib.dll,fveprompt.exe,fvewiz.dll,fveskybackup.dll"
+
+rem manage
+call AddFiles "fvecpl.dll,pcacli.dll,vaultcli.dll"
 
 if "x%opt[shell.app]%"=="xexplorer" (
   rem auto contextmenu
@@ -20,9 +27,15 @@ rem call RegCopy HKLM\System\ControlSet001\Services\BDESVC
 call RegCopy HKLM\Software\Classes\Drive\shell\unlock-bde
 reg add HKLM\Tmp_Software\Classes\Drive\shell\unlock-bde /v Icon /d bdeunlock.exe /f
 
+call RegCopy HKLM\Software\Classes\Drive\shell\encrypt-bde
+call RegCopy HKLM\Software\Classes\Drive\shell\encrypt-bde-elev
+
+call RegCopy HKLM\Software\Classes\Drive\shell\manage-bde
+reg import ManageBitLocker.txt
+
 rem remove unsupported menu
 if "x%opt[build.registry.software]%"=="xfull" (
-  reg delete HKLM\Tmp_Software\Classes\Drive\shell\encrypt-bde-elev /f
+  rem reg delete HKLM\Tmp_Software\Classes\Drive\shell\encrypt-bde-elev /f
 )
 
 set _Need_Fix_Menu=1
